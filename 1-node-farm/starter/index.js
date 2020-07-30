9a289db00d5b6283fed2078cc8be8f01314db779
fuzzyslash <fuzzyslash@hotmail.com>
2020-07-30 14:10:22 -0600

    Overview and Product Done

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,13 +1,101 @@
 const fs = require ('fs');
 const http = require ('http');
+const url = require ('url');
+
 
 
 //////////////////////////
 // SERVER
 //////////////////////////
 
+const replaceTemplate = (template, {id, productName, from, nutrients, image, organic, quantity, price, description}) => {
+
+    template = template.replace (/{%ID%}/g, id);
+    template = template.replace (/{%PRODUCT_NAME%}/g, productName);
+    template = template.replace (/{%FROM%}/, from)
+    template = template.replace (/{%IMAGE%}/g, image);
+    template = template.replace (/{%PRODUCT_NUTRIENTS%}/g, nutrients);
+    template = template.replace (/{%DESCRIPTION%}/g, description);
+    template = template.replace (/{%NOT_ORGANIC%}/g, organic ? '' : 'not-organic');
+    template = template.replace (/{%QUANTITY%}/g, quantity);
+    template = template.replace (/{%PRICE%}/g, price);
+
+    return template;
+
+}
+
+const productsData = fs.readFileSync (`${__dirname}/dev-data/data.json`, 'utf-8');
+const productsJSON = JSON.parse (productsData);
+
+const tempOverview = fs.readFileSync (`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync (`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync (`${__dirname}/templates/template-product.html`, 'utf-8');
+
+
+
 const server = http.createServer ((req, res) => {
-    res.end ('Hello from the server!');
+    const {query, pathname : pathName} = url.parse (req.url, true);
+
+
+    let statusCode = 200;
+    let headers = {"Content-type": "text/html", "my-own-header": "Found!"};
+
+    let response;
+
+    switch (pathName)
+    {
+        //Overview page
+        case "/overview":
+        case "/":
+
+            const cardsHtml = productsJSON.map (el => replaceTemplate (tempCard, el)).join ('');
+
+            response = tempOverview.replace ('{%PRODUCT_CARDS%}', cardsHtml);
+
+            break;
+
+        //Product page
+        case "/product":
+
+            if (query)
+            {
+                const product = productsJSON.find (({id}) => id == query.id);
+
+                response = replaceTemplate (tempProduct, product);
+            }
+            else 
+            {
+                response = 'Product not found.';
+                statusCode = 404;
+            }
+            break;
+
+
+        // API
+        case "/api":
+            
+            headers["Content-type"] = 'application/json';
+            response = productsData;
+
+            break;
+
+        // Not found
+        default:
+            statusCode = 404;
+            headers["my-own-header"] = "Not found :(";
+            response = '<h1>Page not found!</h1>';
+
+    }
+
+    const respond = () => 
+    {
+        res.writeHead (statusCode, headers);
+        res.end (response);
+    }
+
+    if (response)
+        respond ();
+
 })
 
 server.listen (8000, '127.0.0.1', () => {
