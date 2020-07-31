1d0045c3c0cfb46caf8778be7b0f2c947042f984
fuzzyslash <fuzzyslash@hotmail.com>
2020-07-31 09:52:35 -0600

    Promises

---
+++ 3-asynchronous-JS/starter/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const superagent = require('superagent');
+
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject(err);
+      else resolve('Success');
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then(({ body: { message } }) => {
+    return writeFilePro('dog-img.txt', message);
+  })
+  .then((res) => {
+    console.log('Wrote random dog image to file!');
+  })
+  .catch((err) => console.error(err));
